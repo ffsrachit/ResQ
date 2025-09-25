01753640726c9fc85e5383c5e6e9a0491f92a0d9
Rachit Verma <rachit28082004@gmail.com>
2025-09-26 05:28:36 +0530

    Update API endpoints to use production URLs for disaster, alert, donation, resource, volunteer assignment, and user services

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
-export const DISASTER_API_END_POINT = "http://localhost:8000/api/v1/disaster";
-export const ALERT_API_END_POINT = "http://localhost:8000/api/v1/alert";
-export const DONATION_API_END_POINT = "http://localhost:8000/api/v1/donation";
-export const RESOURCE_API_END_POINT = "http://localhost:8000/api/v1/resource";
-export const VOLUNTEER_API_END_POINT = "http://localhost:8000/api/v1/volunteer-assignment";
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const DISASTER_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/disaster";
+export const ALERT_API_END_POINT = "https://resq-2-45pv.onrender.com/v1/alert";
+export const DONATION_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/donation";
+export const RESOURCE_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/resource";
+export const VOLUNTEER_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/volunteer-assignment";
+export const USER_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/user";
