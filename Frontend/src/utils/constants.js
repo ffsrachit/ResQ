8fa6a7e9af2fd75ec12a7acc5dc4c3c4867a621f
Rachit Verma <rachit28082004@gmail.com>
2025-09-26 05:47:10 +0530

    Fix API endpoint for alerts to include the correct path

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const DISASTER_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/disaster";
-export const ALERT_API_END_POINT = "https://resq-2-45pv.onrender.com/v1/alert";
+export const ALERT_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/alert";
 export const DONATION_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/donation";
 export const RESOURCE_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/resource";
 export const VOLUNTEER_API_END_POINT = "https://resq-2-45pv.onrender.com/api/v1/volunteer-assignment";
