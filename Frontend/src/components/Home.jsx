6643eaa99dacd55df766113236ec0309cffc1620
Rachit Verma <rachit28082004@gmail.com>
2025-09-26 05:32:54 +0530

    Fix import path for HomePage component and improve formatting in Home component

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
-import HomePage from './Homepage'
+import HomePage from './HomePage'
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <HomePage/>
+    <HomePage />
     </div>
   )
 }
